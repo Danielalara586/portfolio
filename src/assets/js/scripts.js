11f9a023dc64cffc411ab64b5e550816dfd63d3e
Daniela Lara <laradni@icloud.com>
2022-02-01 20:20:38 -0600

    Add section observer

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -8,3 +8,27 @@ window.addEventListener('scroll', function() {
     else
         menu.classList.remove('menu-fixed');
 })
+
+// Section observer
+const menuLinks = document.querySelectorAll('.nav_links a[href^="#"]');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        const id = entry.target.getAttribute('id');
+        const menuLink = document.querySelector(`.nav_links a[href="#${id}"]`)
+
+        if (entry.isIntersecting) {
+            document.querySelector('.nav_links a.selected').classList.remove('selected');
+            menuLink.classList.add('selected');
+        }
+
+
+    });
+}, { rootMargin: '-50% 0px -50% 0px' });
+
+menuLinks.forEach(menuLink => {
+    const hash = menuLink.getAttribute("href");
+    const target = document.querySelector(hash);
+    if (target)
+        observer.observe(target);
+})
