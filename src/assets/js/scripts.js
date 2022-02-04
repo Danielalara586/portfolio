6aa8a8a09051f29015f2357fb3895746e50a4036
Daniela Lara <laradni@icloud.com>
2022-02-03 20:07:51 -0600

    Implement new functions

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,34 +1,73 @@
-// Sticky menu
-let menu = document.getElementById('nav-menu');
-let height = menu.offsetTop;
+// // Sticky menu
+let menu = null;
+let menuHeight = null;
+const sections = document.querySelectorAll('section');
+const navigation = document.querySelectorAll('.nav_links a[href^="#"]');
 
-window.addEventListener('scroll', function() {
-    if (window.pageYOffset > height)
+window.addEventListener('load', () => {
+    menu = document.getElementById('nav-menu');
+    menu.classList.remove('menu-fixed');
+    window.scrollTo(0, 0);
+    menuHeight = menu.offsetTop;
+});
+
+window.addEventListener('scroll', () => {
+    const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    if (menuHeight && currentScroll >= menuHeight)
         menu.classList.add('menu-fixed');
     else
         menu.classList.remove('menu-fixed');
-})
 
-// Section observer
-const menuLinks = document.querySelectorAll('.nav_links a[href^="#"]');
+    let currentSection = null;
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        const id = entry.target.getAttribute('id');
-        const menuLink = document.querySelector(`.nav_links a[href="#${id}"]`)
-
-        if (entry.isIntersecting) {
-            document.querySelector('.nav_links a.selected').classList.remove('selected');
-            menuLink.classList.add('selected');
+        if (currentScroll >= sectionTop - sectionHeight / 3) {
+            currentSection = section.getAttribute('id');
         }
-
-
     });
-}, { rootMargin: '-50% 0px -50% 0px' });
 
-menuLinks.forEach(menuLink => {
-    const hash = menuLink.getAttribute("href");
-    const target = document.querySelector(hash);
-    if (target)
-        observer.observe(target);
-})
+    navigation.forEach(option => {
+        option.classList.remove('selected');
+
+        if (option.getAttribute('href') === `#${currentSection}`) {
+            option.classList.add('selected');
+        }
+    });
+});
+
+
+// let menu = document.getElementById('nav-menu');
+// let height = menu.offsetTop;
+
+// window.addEventListener('scroll', function() {
+//     if (window.pageYOffset > height)
+//         menu.classList.add('menu-fixed');
+//     else
+//         menu.classList.remove('menu-fixed');
+// })
+
+// // Section observer
+// const menuLinks = document.querySelectorAll('.nav_links a[href^="#"]');
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach(entry => {
+//         const id = entry.target.getAttribute('id');
+//         const menuLink = document.querySelector(`.nav_links a[href="#${id}"]`)
+
+//         if (entry.isIntersecting) {
+//             document.querySelector('.nav_links a.selected').classList.remove('selected');
+//             menuLink.classList.add('selected');
+//         }
+
+
+//     });
+// }, { rootMargin: '-50% 0px -50% 0px' });
+
+// menuLinks.forEach(menuLink => {
+//     const hash = menuLink.getAttribute("href");
+//     const target = document.querySelector(hash);
+//     if (target)
+//         observer.observe(target);
+// })
