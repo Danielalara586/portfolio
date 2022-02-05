865b658aa19160b9dd7127e06b0ceb4d09556660
Daniela Lara <laradni@icloud.com>
2022-02-05 11:40:32 -0600

    Add menu function

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -4,19 +4,23 @@ const navigation = document.querySelectorAll('.nav_links a[href^="#"]');
 let menu = document.getElementById('nav-menu');
 let height = document.querySelector('header').offsetHeight - 150;
 
+function menuLoader() {
+    if (window.pageYOffset > height)
+        menu.style = 'opacity: 1; transition: .5s ease-in';
+    else
+        menu.style.opacity = '0';
+}
+
 // Loader
 window.addEventListener('load', () => {
-    menu.style.opacity = '0';
+    menuLoader();
 
 })
 
 window.addEventListener('scroll', () => {
     const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
     // Dynamic Menu
-    if (window.pageYOffset > height)
-        menu.style = 'opacity: 1; transition: .5s ease-in';
-    else
-        menu.style.opacity = '0';
+    menuLoader();
 
     let sectionId = null;
     sections.forEach(section => {
