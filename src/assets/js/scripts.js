4d81a88100cdd624b7e2ada6bb7ca32eaacf4cfb
Daniela Lara <laradni@icloud.com>
2022-02-04 18:28:23 -0600

    Fix bug 3/3

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,73 +1,38 @@
-// // Sticky menu
-let menu = null;
-let menuHeight = null;
+// Variable init
 const sections = document.querySelectorAll('section');
 const navigation = document.querySelectorAll('.nav_links a[href^="#"]');
+let menu = document.getElementById('nav-menu');
+let height = document.querySelector('header').offsetHeight - 150;
 
+// Loader
 window.addEventListener('load', () => {
-    menu = document.getElementById('nav-menu');
-    menu.classList.remove('menu-fixed');
-    window.scrollTo(0, 0);
-    menuHeight = menu.offsetTop;
-});
+    menu.style.opacity = '0';
+
+})
 
 window.addEventListener('scroll', () => {
     const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-    if (menuHeight && currentScroll >= menuHeight)
-        menu.classList.add('menu-fixed');
+    // Dynamic Menu
+    if (window.pageYOffset > height)
+        menu.style = 'opacity: 1; transition: .5s ease-in';
     else
-        menu.classList.remove('menu-fixed');
+        menu.style.opacity = '0';
 
-    let currentSection = null;
+    let sectionId = null;
     sections.forEach(section => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
 
-        if (currentScroll >= sectionTop - sectionHeight / 3) {
-            currentSection = section.getAttribute('id');
-        }
+        if (currentScroll >= sectionTop - sectionHeight / 3)
+            sectionId = section.getAttribute('id');
+
     });
 
     navigation.forEach(option => {
         option.classList.remove('selected');
 
-        if (option.getAttribute('href') === `#${currentSection}`) {
+        if (option.getAttribute('href') === `#${sectionId}`) {
             option.classList.add('selected');
         }
     });
 });
-
-
-// let menu = document.getElementById('nav-menu');
-// let height = menu.offsetTop;
-
-// window.addEventListener('scroll', function() {
-//     if (window.pageYOffset > height)
-//         menu.classList.add('menu-fixed');
-//     else
-//         menu.classList.remove('menu-fixed');
-// })
-
-// // Section observer
-// const menuLinks = document.querySelectorAll('.nav_links a[href^="#"]');
-
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach(entry => {
-//         const id = entry.target.getAttribute('id');
-//         const menuLink = document.querySelector(`.nav_links a[href="#${id}"]`)
-
-//         if (entry.isIntersecting) {
-//             document.querySelector('.nav_links a.selected').classList.remove('selected');
-//             menuLink.classList.add('selected');
-//         }
-
-
-//     });
-// }, { rootMargin: '-50% 0px -50% 0px' });
-
-// menuLinks.forEach(menuLink => {
-//     const hash = menuLink.getAttribute("href");
-//     const target = document.querySelector(hash);
-//     if (target)
-//         observer.observe(target);
-// })
