1cd2015f0a77c3be335f4a3f28c0a988cbcfaa6e
Daniela Lara <laradni@icloud.com>
2022-02-08 21:54:38 -0600

    Add filter function

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -57,3 +57,28 @@ skillBars.forEach(skillBar => {
         bar.style.transition = '.8s';
     }
 });
+
+// Portfolio filter
+const filterButtons = document.querySelectorAll('.filter');
+const items = document.querySelectorAll('.item');
+
+for (let i = 0; i < filterButtons.length; i++) {
+    filterButtons[i].addEventListener("click", function() {
+        for (let j = 0; j < filterButtons.length; j++) {
+            filterButtons[j].classList.remove("active")
+        }
+        this.classList.add("active");
+        const target = this.getAttribute("data-target")
+
+        for (let k = 0; k < items.length; k++) {
+            items[k].style.display = "none";
+            if (target == items[k].getAttribute("data-id")) {
+                items[k].style.display = "block";
+            }
+            if (target == "all") {
+                items[k].style.display = "block";
+            }
+        }
+
+    })
+}
