a82d291f2c6289d2052199ea09fe4262c8240d8c
Daniela Lara <laradni@icloud.com>
2022-02-09 12:05:15 -0600

    Add carousel function

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -57,3 +57,48 @@ skillBars.forEach(skillBar => {
         bar.style.transition = '.8s';
     }
 });
+
+// Carousel 
+let slidePosition = 0;
+const slides = document.getElementsByClassName('carousel-items');
+const totalSlides = slides.length;
+
+document.
+getElementById('next')
+    .addEventListener("click", function() {
+        moveToNextSlide();
+    });
+document.
+getElementById('prev')
+    .addEventListener("click", function() {
+        moveToPrevSlide();
+    });
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('visible');
+        slide.classList.add('hidden');
+    }
+
+    slides[slidePosition].classList.add('visible');
+}
+
+function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+
+    updateSlidePosition();
+}
